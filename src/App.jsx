a873068974c5f5a9b2fbe505a8f6e17d735a3b29
ryan <ryanmarchock@gmail.com>
2018-11-11 22:34:42 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-import React, {Component} from 'react';
+import { Route, Switch } from "react-router-dom";
+import React from 'react'
 
-class App extends Component {
-  render() {
+
+import Home from './Home.jsx';
+import Contact from './Contact.jsx';
+import About from './About.jsx';
+import Header from './Header.jsx';
+
+const App = () => {
+    const someVariable = true;
+
     return (
-      <h1>Hello React :)</h1>
+        <div>
+            <Header />
+            <Switch>
+                {/* these are good */}
+                <Route exact path="/" component={Home} />
+                <Route
+                    path="/about"
+                    render={props => <About {...props} extra={someVariable} />}
+                />
+                {/* do not do this */}
+                <Route
+                    path="/contact"
+                    component={props => <Contact {...props} extra={someVariable} />}
+                />
+            </Switch>
+        </div>
     );
-  }
-}
-export default App;
+};
+
+export default App
+
+
